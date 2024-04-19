1111a35434c106aa8c6132fcb31e6cabfc63b276
satoutahhaithem <satoutahhaithem>
2024-04-19 17:43:40 +0200

    do the main structure of the repository

---
+++ Data/ROADEF2021.py
@@ -0,0 +1,13 @@
+conference_sessions = 27
+slots = 11
+papers_range = [3,4,5]
+working_groups = 17 
+np= [11, 11, 8, 8, 8, 8, 10, 10, 13, 7, 4, 7, 10, 6, 5, 4, 5, 5, 5, 7, 4, 4, 6, 3,4, 4, 5]
+
+npMax = [4,5,3,3,4,3,5,4,4,3,5]
+
+# Define the working groups associated with each session
+session_groups = [
+[9], [4], [9], [2], [3], [14], [10], [4], [12], [11], [4], [6], [7], [4],[1],[16],
+[13] , [4] , [17] , [16] ,[17] , [17] , [9] , [] ,[3,8] ,[15,5] , [6,4]
+]
